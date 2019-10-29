feb66619e3795f2c670ad4517d0efabf0eb4ad21
tvmota <tiago.vieira.mota@gmail.com>
2019-10-28 23:48:33 -0300

    Commit inicial

---
+++ src/App.svelte
@@ -0,0 +1,48 @@
+<script>
+  import Icon from './components/common/icon/Icon.svelte';
+</script>
+<div class="grid-box">
+  <header class="grid-header" role="header">
+    <h1>Header</h1>    
+  </header>
+  <main class="grid-main">
+    <div class="teste bg-black"></div>
+    <Icon iconName="pkmn"/>
+  </main>
+  <footer class="grid-footer" role="content-info">
+    <h2>footer</h2>
+  </footer>
+</div>
+
+<style>
+  h1 {
+    color: purple;
+  }
+
+  .teste {
+    height: 40px;
+  }
+
+  .grid-box {
+    width: 100%;
+    margin: 0 auto;
+    min-height: 100vh;
+    display: grid;
+    grid-gap: 10px;
+    grid-template-columns: auto;
+    grid-template-rows: auto 1fr auto;
+    grid-template-areas: "header header header" "main main main" "footer footer footer";
+  }
+
+  .grid-main {
+    grid-area: main;
+  }
+
+  .grid-header {
+    grid-area: header;  
+  }
+
+  .grid-footer {
+    grid-area: footer;
+  }
+</style>
---
+++ src/components/common/icon/Icon.svelte
@@ -0,0 +1,16 @@
+<script>
+  import { beforeUpdate } from 'svelte';
+  import * as icons from '../../../assets/icons/*.svg';
+  export let iconName;
+</script>
+
+<svg>
+  <use xlink:href={icons[iconName]}/>
+</svg>
+
+<style>
+  svg {
+    height: 40px;
+    width: 40px;
+  }
+</style>
