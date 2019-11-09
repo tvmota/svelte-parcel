de673a56f2a5b9ff06e626db444dc42dc90b8b8b
tvmota <tiago.vieira.mota@gmail.com>
2019-11-09 00:39:40 -0300

    Adiçao de descricaçao na tela de detalhes

--- src/App.svelte
+++ src/App.svelte
@@ -8,7 +8,7 @@
 
 <div class="grid-box">
   <AppHeader bgColor="bg-red-600" headerTitle="SvelDex" txtColor="text-gray-100"/>
-  <main class="grid-main pb-8 flex justify-center">
+  <main class="grid-main pb-8 px-4 flex justify-center">
     <Router>
       <Route exact path="/" component={Home} />
       <Route exact path="/detail/:id" component={Detail} />
--- src/pages/Detail.svelte
+++ src/pages/Detail.svelte
@@ -6,22 +6,29 @@
   export let router = {};
   let pkmn;
   let pkmnSpecie;
+  let description;
   let promise = pkmnInfo();
 
   async function pkmnInfo() {
     const resp = await Promise.all([getPkmn(router.params.id), getSpecies(router.params.id)]);
     pkmn = resp[0];
     pkmnSpecie = resp[1];
-
+    description = getDescription(pkmnSpecie);
     return Promise.resolve(resp);
   };
+
+  function getDescription(specie) {
+    return specie.flavor_text_entries
+      .find(entrie => entrie.language.name === 'en')
+      .flavor_text;
+  }
 </script>
 <div>
   {#await promise}
     <Loader />
   {:then val}
-    <div class="max-w-6xl mx-auto flex flex-col border py-8">
-      <div class="flex">
+    <div class="max-w-4xl mx-auto flex flex-col border py-8">
+      <div class="flex justify-center">
         <img
           alt={pkmn.name}
           class="w-64 h-64"
@@ -31,8 +38,8 @@
           class="w-64 h-64"
           src={pkmn.sprites.back_default}>
       </div>
-      <div class="flex mx-10 bg-red-300 py-2 justify-around">
-        <h1 class="capitalize text-2xl">
+      <div class="flex mx-10 bg-red-300 py-2 justify-around mb-6">
+        <h1 class="capitalize text-2xl font-medium">
           {`#${pkmn.id} ${pkmn.name}`}
         </h1>
         <div>
@@ -43,6 +50,22 @@
           {/each}
         </div>
       </div>
+      <hr class="my-3 hr--text relative text-center" data-content="Info">
+      <div class="flex mx-10">
+        <blockquote class="border-l-4 border-red-300 px-4">
+          <cite class="font-bold">Description</cite>
+          <p class="mt-2 italic">{description}</p>
+        </blockquote>
+      </div>
     </div>
   {/await}
 </div>
+
+<style>
+  .hr--text::after {
+    content: attr(data-content);
+    position: absolute;
+    display: inline-block;
+    transform: translateY(-.76rem);
+  }
+</style>
